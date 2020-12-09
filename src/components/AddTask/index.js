ae3081661f82f7fca0dcbca22105b4ff37653bfd
Mykhailo <mike.girnyak21@gmail.com>
2020-12-09 18:37:16 +0200

    fixed add task button

--- src/components/AddTask/index.js
+++ src/components/AddTask/index.js
@@ -32,29 +32,29 @@ const AddTask = ({ id }) => {
     <div className={s.addtask}>
       {!isVisibleForm ? (
         <div className={s.addtask_open}>
-          <svg
-            width="14"
-            height="14"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8 1V15"
-              stroke="#62dafc"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M1 8H15"
-              stroke="#62dafc"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
           <button type="button" onClick={toogleFormVisible}>
+            <svg
+              width="14"
+              height="14"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8 1V15"
+                stroke="#62dafc"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M1 8H15"
+                stroke="#62dafc"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
             Add Task
           </button>
         </div>
